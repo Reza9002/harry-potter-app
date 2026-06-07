6e5d3b25f0e127f99928aaa8696cbf6830a605e6
Reza9002 <rezadawari@yahoo.com>
2026-06-07 16:12:44 +0200

    add analytics punkt

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import FetchExample from './components/FetchExample.jsx';
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from '@vercel/analytics/react';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 	<FetchExample />
-	<Analytics/>
-	</React.StrictMode>
+<Analytics />	
+</React.StrictMode>
 );
 
 
