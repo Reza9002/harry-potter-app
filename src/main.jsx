1cea9a702791b42c544f95e71c50ab9fd55b2258
Reza9002 <rezadawari@yahoo.com>
2026-06-07 13:14:33 +0200

    add analytistic

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import FetchExample from './components/FetchExample.jsx';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 
 
