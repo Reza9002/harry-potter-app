8abe35c2e6a8fa78615eb7c4f81bdae7ba6008f1
Reza9002 <rezadawari@yahoo.com>
2026-06-07 12:57:58 +0200

    add  vercel analytics to Harryy Potter

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import FetchExample from './components/FetchExample.jsx';
+import { Analytics } from "@vercel/analytics/react"
 
 
 
@@ -9,6 +10,7 @@ import FetchExample from './components/FetchExample.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 	<FetchExample />
+	<Analytics/>
 	</React.StrictMode>
 );
 
